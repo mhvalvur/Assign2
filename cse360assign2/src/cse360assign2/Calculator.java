4b89730c886e09f8e46969207ec8ea758ef478f6
DESKTOP-AD3B4NI\Markus Valvur <markus.valvur@prodigylabs.net>
2019-02-25 20:18:05 -0700

    Finished part 3 of assignment 2

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,4 +1,5 @@
 // Author: Markus Valvur
+// Class ID: 574
 // Description: Calculator class for assignment 2
 
 package cse360assign2;
@@ -6,9 +7,11 @@ package cse360assign2;
 public class Calculator {
 	
 	private int total;
+	private String history;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	public int getTotal () {
@@ -17,14 +20,17 @@ public class Calculator {
 	
 	public void add (int value) {
 		total += value;
+		history += " + " + value;
 	}
 	
 	public void subtract (int value) {
 		total -= value;
+		history += " - " + value;
 	}
 	
 	public void multiply (int value) {
 		total *= value;
+		history += " * " + value;
 	}
 	
 	public void divide (int value) {
@@ -32,9 +38,10 @@ public class Calculator {
 		else {
 			total = total/value;
 		}
+		history += " / " + value;
 	}
 	
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
